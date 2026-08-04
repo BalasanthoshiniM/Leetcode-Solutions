79d401ff9d28af1a76055d8a60316837e25daf11
Balasanthoshini M <23csea05balasanthoshini@gmail.com>
2026-08-04 05:36:03 +0530

    Time: 4 ms (4.88%), Space: 46 MB (5.09%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,31 +1,35 @@
 class Solution {
     public String decodeString(String s) {
         Stack <Character> stack=new Stack<>();
-        String k="";
-        String str="";
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)!=']'){
                 stack.push(s.charAt(i));
             }
             else{
-                str="";
-                while(stack.peek()!='['){
+                String str="";
+                String res="";
+                String num="";
+                while(!stack.isEmpty() && stack.peek()>='a' && stack.peek()<='z'){
                     str=stack.pop()+str;
                 }
-                stack.pop();
-                k="";
-                while(!stack.isEmpty()&& Character.isDigit(stack.peek())){
-                    k=stack.pop()+k;
+                if(stack.peek()=='[' ){
+                    stack.pop();
                 }
-                int n=Integer.parseInt(k);
-                String res="";
-                for(int j=1;j<=n;j++){
-                res+=str;
+                while(!stack.isEmpty() && stack.peek() >='0' && stack.peek()<='9'){
+                    num=stack.pop()+num;
                 }
-                for(char c: res.toString().toCharArray()){
-                    stack.push(c);
+                int times=0;
+                if(!num.isEmpty()){
+                    times=Integer.parseInt(num);
+                }
+                for(int k=1;k<=times;k++){
+                    res=str+res;
+                }
+                for(int k=0;k<res.length();k++){
+                    stack.push(res.charAt(k));
                 }
             }
+
         }
         String result="";
         while(!stack.isEmpty()){
